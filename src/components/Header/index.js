d80b0beb373a4ecd54d9793d110d0be126722fb4
crackedwindow <bathzip@gmail.com>
2020-02-09 22:59:20 +0000

    update project 2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React from 'react'
+import './style.css'
 /**
 * @author
 * @function Header
@@ -7,12 +7,15 @@ import React from 'react';
 
 const Header = (props) => {
   return(
-    <header>
-        <nav>
+    <header className="header">
+        <nav className="headerMenu">
             <a href="#">Home</a>
             <a href="#">About</a>
             <a href="#">Contact</a>
         </nav>
+        <div>
+          social media links
+        </div>
     </header>
    )
 
