fcb50833548b670ce07699006bbcebbfbc66bee0
crackedwindow <bathzip@gmail.com>
2020-02-09 12:37:42 +0000

    new update

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/**
+* @author
+* @function Header
+**/
+
+const Header = (props) => {
+  return(
+    <header>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </nav>
+    </header>
+   )
+
+ }
+
+export default Header
+
